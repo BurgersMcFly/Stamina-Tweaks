9ce8f70f6f3f5dec0641db599d76e712cd907e02
BurgersMcFly <burgersmcfly@outlook.com>
2023-03-29 22:08:03 +0300

    Update StaminaTweaks.cs

--- StaminaTweaks.cs
+++ StaminaTweaks.cs
@@ -1,5 +1,5 @@
-﻿// StaminaTweaks, Valheim mod that improves stamina
-// Copyright (C) 2022 BurgersMcFly
+// StaminaTweaks, Valheim mod that improves stamina
+// Copyright (C) 2023 BurgersMcFly
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
